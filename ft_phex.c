435590da3df9c175bde31266307cf2ef914d11c6
MR Khan <khan.m.cloud@gmail.com>
2023-06-02 00:00:52 -0400

    print hex function

--- ft_phex.c
+++ ft_phex.c
@@ -6,14 +6,16 @@
 /*   By: makhan <makhan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 00:46:30 by makhan            #+#    #+#             */
-/*   Updated: 2023/05/31 00:49:24 by makhan           ###   ########.fr       */
+/*   Updated: 2023/06/01 23:59:54 by makhan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-//
+//Function to print number in Hexadecimal (base 16) format
+//ft_print_hexadecimal_format i.e ft_phex
 int ft_phex(int c, int ascii)
 {
+
 	return 0;
 }
